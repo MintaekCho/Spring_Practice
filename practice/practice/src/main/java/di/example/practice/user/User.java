fef9ca93eae304666f9852f2cf93740c48328ba6
Mintaek_CHO <mintaek5555@gmail.com>
2022-08-10 23:16:54 +0900

    Spring practice

---
+++ practice/practice/src/main/java/di/example/practice/user/User.java
@@ -0,0 +1,7 @@
+package di.example.practice.user;
+
+public class User {
+    private Long id;
+    private String name;
+    private UserGrade userGrade;
+}
---
+++ practice/practice/src/main/java/di/example/practice/user/UserGrade.java
@@ -0,0 +1,7 @@
+package di.example.practice.user;
+
+public enum UserGrade {
+    GRADE_0,
+    GRADE_1,
+    GRADE_2,
+}
